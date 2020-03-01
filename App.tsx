4110493594764c215126770e82bcdfdefe0edc7f
fayded <kevin.d.fay@gmail.com>
2020-03-01 15:25:50 -0500

    before adding graphQL api

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import { withAuthenticator } from 'aws-amplify-react-native'
-
 import Amplify from '@aws-amplify/core'
 import config from './aws-exports'
 Amplify.configure(config)
