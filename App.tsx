bcb216e27f307d35aa6d32f182c185ae437c609d
fayded <kevin.d.fay@gmail.com>
2020-03-02 08:52:44 -0500

    auth creds beginning

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Auth } from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react-native'
 import Amplify from '@aws-amplify/core'
 import config from './aws-exports'
 Amplify.configure(config)
 
 class App extends React.Component {
+  state = {
+    user: {}
+  }
+  getUserData() {
+    return Auth.currentCredentials();
+  }
+  componentDidMount() {
+   this.getUserData();
+  }
   render() {
     return (
       <View style={styles.container}>
